47583cb6247547cb0f80318f77cb649c85ceda96
Harshad Riswadkar <riswadkar.harshad@gmail.com>
2017-07-09 16:53:35 +0530

    added FloydTriangle.java

---
+++ FloydTriangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class FloydTriangle {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the number of rows which you want in your Floyd Triangle: ");
+		int r = in.nextInt();
+		System.out.println("r is %d" + r);
+		int n=0;
+		for(int i=0; i<r; i++) {
+			for(int j=0; j<=i; j++) {
+				System.out.print(++n+" ");
+			}
+			System.out.println();
+		}
+	}
+}
